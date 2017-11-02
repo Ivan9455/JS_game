457b7f997722bdda76bb154b6894d1cec0a7ffc9
Ivan9455 <paderin94126@gmail.com>
2017-11-02 14:12:32 +0700

    сделал рабочие часы доработал их)

--- my_clock/js/script.js
+++ my_clock/js/script.js
@@ -1,16 +1,16 @@
 window.onload = function () {
     setInterval(clock,100);
-}
+
+};
 
 function clock() {
     var result = document.getElementById("result");
     var time = new Date();
     var year = time.getFullYear();
     var month = time.getMonth();
-    var day = time.getDate();
-    var h = time.getHours();
-    var m = time.getMinutes();
-    var s = time.getSeconds();
+    var day = (time.getDate().toString().length==1)? "0" + time.getDate():time.getDate();
+    var h = (time.getHours().toString().length==1)? "0" + time.getHours():time.getHours();
+    var m = (time.getMinutes().toString().length==1)? "0" + time.getMinutes():time.getMinutes();
+    var s = (time.getSeconds().toString().length==1)? "0" + time.getSeconds():time.getSeconds();
     result.innerHTML = day + "." + month + "." + year +"<br>" +h + ":" + m + ":" + s;
-
 }
